b2b2188ffc54f227c1fe9940d88648e32269da92
BossLakryM <bosslakrym@gmail.com>
2023-11-08 15:55:27 +0100

    my header from ground up

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Header.css';
+import './styles/Header.css';
 import logo from "../assets/youtube-logo.svg";
 import hamburger from "../assets/hamburger-menu.svg";
 import notifications from "../assets/notifications.svg";
@@ -11,27 +11,27 @@ import apps from "../assets/youtube-apps.svg";
 const Header = () => {
   return (
     <div className='header'>
-      <div className="right-column">
-        <img className='harmBurger' src={hamburger} />
-        <img className='logo' src={logo} />
-
+      <div className="left-column">
+        <img className='hamburger' src={hamburger}
+        />
+        <img className='logo' src={logo}
+        />
       </div>
       <div className="middle-column">
-        <input className='search' placeholder='Search' type='text' />
-        <button className='searchBtn'>
+        <input className='searchBar' type='text' placeholder='Search' />
+        <button className='searchButton'>
           <img className='searchIcon' src={search} />
         </button>
-        <button className='voice-btn'>
+        <button className='voiceButton'>
           <img className='voiceIcon' src={voice} />
         </button>
       </div>
-      <div className="left-column">
-        <img className='upload' src={upload} />
-        <img className='ytApps' src={apps} />
-        <img className='notify' src={notifications} />
-        <img className='profilePic' src='https://supersimple.dev/public/img/exercises/youtube/channel-profile-pics/my-channel.jpeg' />
+      <div className="right-column">
+        <img src={upload} className='uploadIcon' />
+        <img src={apps} className='ytApps' />
+        <img src={notifications} className='notify' />
+        <img src="https://supersimple.dev/public/img/exercises/youtube/channel-profile-pics/my-channel.jpeg" className='channelIcon' />
       </div>
-
     </div>
   )
 }
